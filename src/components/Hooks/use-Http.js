406c23568aad6180a8fe9225fcfdddbb59691d68
imakhlaq <imakhlaqXD@gmail.com>
2022-12-22 17:55:07 +0530

    custom hooks

--- src/components/Hooks/use-Http.js
+++ src/components/Hooks/use-Http.js
@@ -1,36 +1,39 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 //taking requestconfig to config fetch and a fuch to pass data to that function
 const useHttp = (applydataFun) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = async (requestConfig) => {
-    try {
-      setIsLoading(true);
-      setError(null);
+  const sendRequest = useCallback(
+    async (requestConfig) => {
+      try {
+        setIsLoading(true);
+        setError(null);
 
-      //request
+        //request
 
-      const res = await fetch(requestConfig.url, {
-        method: requestConfig.method ? requestConfig.method : "GET",
-        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
-        headers: requestConfig.headers ? requestConfig.headers : {},
-      });
+        const res = await fetch(requestConfig.url, {
+          method: requestConfig.method ? requestConfig.method : "GET",
+          body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+          headers: requestConfig.headers ? requestConfig.headers : {},
+        });
 
-      //checking for response
-      if (!res.ok) {
-        throw new Error("SomeThing Went Wrong");
+        //checking for response
+        if (!res.ok) {
+          throw new Error("SomeThing Went Wrong");
+        }
+        const data = await res.json();
+
+        //passing data to this function so this function do whatever it need whith that data
+        applydataFun(data, requestConfig.text ? requestConfig.text : null);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
       }
-      const data = await res.json();
-
-      //passing data to this function so this function do whatever it need whith that data
-      applydataFun(data, requestConfig.text ? requestConfig.text : null);
-    } catch (err) {
-      setError(err.message);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+    },
+    [applydataFun]
+  );
   return [isLoading, error, sendRequest];
 };
 
